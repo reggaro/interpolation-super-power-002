32327341a6bcefc5b44d0755351df7cf4e470a9d
reggaro <reggiearo@gmail.com>
2016-01-25 18:49:44 +0000

    Done.

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -1,3 +1,5 @@
-def display_rainbow
-  puts "R: red, O: orange, Y: yellow, G: green, B: blue, I: indigo, V: violet"
+def display_rainbow(color = "red")
+  puts "R: #{color[0]}, O: #{color[1]}, Y: #{color[2]}, G: #{color[3]}, B: #{color[4]}, I: #{color[5]}, V: #{color[6]}"
 end
+
+display_rainbow
---
+++ lib/rainbowtest1.rb
@@ -0,0 +1,7 @@
+def greeting_programmer(name, language)
+  puts "Hello, #{name}. We heard you are a great #{language} programmer."
+end
+
+greeting_programmer("Sophie", "Ruby")
+
+greeting_programmer("Steve", "Elixir")
